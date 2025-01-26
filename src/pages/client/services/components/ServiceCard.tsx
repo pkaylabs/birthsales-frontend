4a0f5dd38388fc9f58042bd8094ff02ead8bc337
unknown <jamalnasirone52@gmail.com>
2025-01-26 17:07:08 +0000

    services page edited

--- src/pages/client/services/components/ServiceCard.tsx
+++ src/pages/client/services/components/ServiceCard.tsx
@@ -14,26 +14,28 @@ const ServiceCard = ({
   endPrice: number;
 }) => {
   return (
-    <div className="w-[300px] p-4 mb-6 rounded-xl relative hover:scale-90 tranform transition-transform duration-500 ease-in-out shadow-lg">
-      <div className="rounded-xl h-2/3">
+    <div className="w-[300px] flex flex-col justify-between p-4 mb-6 rounded-xl relative hover:scale-90 tranform transition-transform duration-500 ease-in-out shadow-lg">
+      <div className="rounded-xl h-2/3 bg-slate-100">
         <img
           src={img}
           alt="thumbnails"
           className="w-full object-contain h-full rounded-xl"
         />
       </div>
-      <div>
-        <h1 className="font-medium text-xl">{title}</h1>
-      </div>
-      {/* a faint line */}
+      <div className="flex flex-col ">
+        <div>
+          <h1 className="font-medium text-xl">{title}</h1>
+        </div>
+        {/* a faint line */}
 
-      <div className="">
-        <p className="font-normal text-base text-gray-400">{description}</p>
-      </div>
-      <div className="">
-        <p className="font-medium">
-          ${startPrice} - ${endPrice}
-        </p>
+        <div className="">
+          <p className="font-normal text-base text-gray-400">{description}</p>
+        </div>
+        <div className="">
+          <p className="font-medium">
+            ${startPrice} - ${endPrice}
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/pages/client/services/index.tsx
+++ src/pages/client/services/index.tsx
@@ -32,12 +32,12 @@ const Services = () => {
 
           <ServiceCard
             img={
-              "https://s3-alpha-sig.figma.com/img/288e/0133/65fe639fccc1fe4168fca740ef1f85e7?Expires=1737936000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=HNk7VuWjqrbNmMsYUH0EWdMt7Z1Ndf7Nv597FjsVS70K4oPYFXWeqPnNb3j83rYmhqncv1PWWlJICu14dGiSwRBvcWUqJRTqMNtGLCSmdngToYWbBck24~rCvvQqADEfkCyz4qClf93MjSSjkRWFw3RgdkE4S03I3tOU-Hz1XgG1JH00bnA8hSD1e-ywZnwvyErJBRJ-PoCN2nLyXsPZn-xCVZmKNKeus~e0IKkKD9Z9mYR7Hzu3KNumobE6fHxJb9u244sId6cZS7J4lbz9az7c4kRUTmiBJ4EaKCfPCwCRRjsZBL-mWeozn7CzoBCz7zQTpXldgOijypVsxLZLVg__"
+              "https://s3-alpha-sig.figma.com/img/4f3c/a1d1/2722dbdf98f25179d3c0b785988c513d?Expires=1737936000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=iA0crnI392D3e3Z9VeDwKwWdCbEy7mX-fgABU1fMN00imHJIuxqM4DZwmKNEwwdnEqC9Q6n7Oo3WhezM9jRMwkMABxo79w6UbK9w~MGWeqbPDk52JZY1gqK~OWguGU4VfEaugNwutnOD5NVuZ~AiEhInbF8puYiDqH7hlkw7~ULvVNMoRPtSURnSFEdxGg4eh8xm72wsYTAHFtTUXOUKH-LSZeyhavH3CcWgP6yF5pMmIVUJbt4dIN0bVCWKnRDSfM8vcYQb7QHzy6lxS7uAy4zTAiJlUOSsPLvgIzYwGckssk5YmQMZ3CBwbbeQ-C1PEjaTdNCp~dAVtA5SL4~uzw__"
             }
-            title="Games"
+            title="Bags"
             description="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-            startPrice={150}
-            endPrice={360}
+            startPrice={50}
+            endPrice={100}
           />
           <ServiceCard
             img={
@@ -59,7 +59,7 @@ const Services = () => {
           />
           <ServiceCard
             img={
-               "https://s3-alpha-sig.figma.com/img/04a1/915f/d6cedd7c8b1073685c5f1be1b50e1ac6?Expires=1737936000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=EtgMC9jiFrs0EcWRDmVWwGT6gLVeThTBS~80pL1XCZYPuH-W71vSgj7sirtDajsg8NRNWEZyaDxq0xWcB8lA0uHgLzAclnfHNRbwRHFdzFkbNgfZGApPJy2VHl0ftm07LOa68Ulomu3etHonG864xn9TmuXMt1OhM3Ym2DjCn7VkISiwh4N7SXXjupIN3XcF~gMH~JWxzOTucGx-YFssWW4PiDqhxp0eDgEEJ1YvSnehX5sDpXyZYpEnB689udpVMNTfHyJJZLGWidT2zeo0KYloHKzBTQKnwr-AH0ZwXh6B5MJ33XFlWYwXX5zGxPtCxC4ui66ez0CcbCp6x08SrQ__"
+              "https://s3-alpha-sig.figma.com/img/04a1/915f/d6cedd7c8b1073685c5f1be1b50e1ac6?Expires=1737936000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=EtgMC9jiFrs0EcWRDmVWwGT6gLVeThTBS~80pL1XCZYPuH-W71vSgj7sirtDajsg8NRNWEZyaDxq0xWcB8lA0uHgLzAclnfHNRbwRHFdzFkbNgfZGApPJy2VHl0ftm07LOa68Ulomu3etHonG864xn9TmuXMt1OhM3Ym2DjCn7VkISiwh4N7SXXjupIN3XcF~gMH~JWxzOTucGx-YFssWW4PiDqhxp0eDgEEJ1YvSnehX5sDpXyZYpEnB689udpVMNTfHyJJZLGWidT2zeo0KYloHKzBTQKnwr-AH0ZwXh6B5MJ33XFlWYwXX5zGxPtCxC4ui66ez0CcbCp6x08SrQ__"
             }
             title="Shirts"
             description="Lorem ipsum dolor sit amet consectetur adipisicing elit."
@@ -68,7 +68,7 @@ const Services = () => {
           />
           <ServiceCard
             img={
-               "https://s3-alpha-sig.figma.com/img/e59d/9f34/8cc24eeff489863523b63971c3ff8e4a?Expires=1737936000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=FSE78Tz65VlRUa-zDh5rc5FzVbnlnot2ZPm4CpqwvLMcskCBZX0xYfv-9PsAv2n-NjX6Qe8IUh9hGtMtwkXpDGpzE25i6LQpOFx0dHwu~l5iTH1WnHQo3W9hJSEXqKtqJ~KQyqpOKGvJ32pTcQNLZuOfZiSW6adSC5YkodQs9243pOEohF6YyJdhQeqAIk36pv36de0o~A8QR1ATC5x644aj6SVMazXC7sSElN38avZc2cAq8yZ7bhiPyEVvL0MYpxEuiP9RpAsgyVnHisow9hXhnuL7YYMte6Bfnr1RfXlxZNVAxz9ROMQUeh~RbJHunSVDLFXBztmKZm9pL9ClgA__"
+              "https://s3-alpha-sig.figma.com/img/e59d/9f34/8cc24eeff489863523b63971c3ff8e4a?Expires=1737936000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=FSE78Tz65VlRUa-zDh5rc5FzVbnlnot2ZPm4CpqwvLMcskCBZX0xYfv-9PsAv2n-NjX6Qe8IUh9hGtMtwkXpDGpzE25i6LQpOFx0dHwu~l5iTH1WnHQo3W9hJSEXqKtqJ~KQyqpOKGvJ32pTcQNLZuOfZiSW6adSC5YkodQs9243pOEohF6YyJdhQeqAIk36pv36de0o~A8QR1ATC5x644aj6SVMazXC7sSElN38avZc2cAq8yZ7bhiPyEVvL0MYpxEuiP9RpAsgyVnHisow9hXhnuL7YYMte6Bfnr1RfXlxZNVAxz9ROMQUeh~RbJHunSVDLFXBztmKZm9pL9ClgA__"
             }
             title="Laptops"
             description="Lorem ipsum dolor sit amet consectetur adipisicing elit."
